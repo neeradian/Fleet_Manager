0c4870d8348586f3ff3a3eec26330bf3c00be8cc
neeradian <nimeshmandal9212@gmail.com>
2026-04-07 13:57:43 +0530

    started app "vehicle"

--- fleet_manager/settings.py
+++ fleet_manager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-r%&u!-@-ix&xz@!^8^w_3+1by7*vhf6pd__%ryoixm6r&c*p^s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
