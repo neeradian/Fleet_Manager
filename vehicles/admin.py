ff8aa3496adf64eaaf483cb2fb78d45198e8d42c
neeradian <nimeshmandal9212@gmail.com>
2026-04-16 22:58:56 +0530

    added permission to admin user

--- vehicles/admin.py
+++ vehicles/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-
-from vehicles.models import User, Vehicle
+from .models import User, Vehicle, RTODetail, TyreRecord, ServiceRecord, FuelRecord
 
 
 # Register your models here.
@@ -27,3 +26,52 @@ class VehicleAdmin(admin.ModelAdmin):
         "created_by",
     ]
     readonly_fields = ["created_by", "updated_by", "created_at", "updated_at"]
+
+
+@admin.register(RTODetail)
+class RTODetailAdmin(admin.ModelAdmin):
+    list_display = [
+        "vehicle",
+        "rto_office",
+        "registration_date",
+        "insurance_valid_upto",
+    ]
+    readonly_fields = ["created_by", "updated_by"]
+
+
+@admin.register(TyreRecord)
+class TyreRecordAdmin(admin.ModelAdmin):
+    list_display = [
+        "vehicle",
+        "changed_date",
+        "km_at_change",
+        "tyre_position",
+        "created_by",
+    ]
+    readonly_fields = ["created_by", "updated_by"]
+
+
+@admin.register(ServiceRecord)
+class ServiceRecordAdmin(admin.ModelAdmin):
+    list_display = [
+        "vehicle",
+        "service_date",
+        "km_at_service",
+        "total_cost",
+        "created_by",
+    ]
+    readonly_fields = ["created_by", "updated_by"]
+
+
+@admin.register(FuelRecord)
+class FuelRecordAdmin(admin.ModelAdmin):
+    list_display = [
+        "vehicle",
+        "fill_date",
+        "km_at_fill",
+        "litres_filled",
+        "total_amount",
+        "mileage",
+        "created_by",
+    ]
+    readonly_fields = ["total_amount", "mileage", "created_by", "updated_by"]
