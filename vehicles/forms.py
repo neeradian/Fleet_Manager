6c6fb504f955245419aa475b1ef3315325694c47
neeradian <nimeshmandal9212@gmail.com>
2026-04-16 22:02:50 +0530

    added user form

--- vehicles/forms.py
+++ vehicles/forms.py
@@ -1,8 +1,40 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
+from .models import (User)
 
 # ------------------------------------- Login Form -----------------------------------------
 
 class LoginForm(AuthenticationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username', 'autofocus': True}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+
+
+
+class UserCreateForm(forms.ModelForm):
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput(attrs={'placeholder': 'Confirm password'}))
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'role']
+
+    def clean_password2(self):
+        p1, p2 = self.cleaned_data.get('password1'), self.cleaned_data.get('password2')
+        if p1 and p2 and p1 != p2:
+            raise forms.ValidationError("Passwords don't match.")
+        return p2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+        return user
+
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'role', 'is_active']
+
+
